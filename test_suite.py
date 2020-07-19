d1f7bb9d3629716b89ce047294bdfd54354eb7c7
yanjinzhu <40293998+yanjinzhu@users.noreply.github.com>
2020-07-19 11:43:28 +0800

    Update test_suite.py

--- test_suite.py
+++ test_suite.py
@@ -9,7 +9,8 @@ from config import HEADERS
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(LogninTest))
 suite.addTest(unittest.makeSuite(AddGood))
-report_file = "./report/test-{}.html".format(time.strftime("%Y%m%d %H%M%S"))
+#report_file = "./report/test-{}.html".format(time.strftime("%Y%m%d %H%M%S"))
+report_file = "./report/test.html"
 
 with open(report_file,"wb") as f:
     runner = HTMLTestRunner(f, title="宇泽购物流程－测试报告",description="v0.1")
